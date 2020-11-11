b7f1c5f938b972abbccbdd3e7be73eb69d988082
Magno Sescon <magnosescon@gmail.com>
2020-11-11 17:48:18 -0300

    test: ensure RemoteAuthentications throw UnexpectedError id HttpPostClient returns 500

--- src/data/usesCases/authentication/remote-authentication.spec.ts
+++ src/data/usesCases/authentication/remote-authentication.spec.ts
@@ -61,4 +61,12 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthentication())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+  it('should throw UnexpectedError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.auth(mockAuthentication())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })

6d69bae2efdf412d5416e66119213d97088a0107
Magno Sescon <magnosescon@gmail.com>
2020-12-14 13:16:51 -0300

    feat: ensure Emailvalidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,7 +5,7 @@ export class EmailValidation implements FieldValidation {
   constructor (readonly field: string) {}
 
   validate (value: string): Error {
-    console.log(value)
-    return new InvalidFieldError()
+    const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    return emailRegex.test(value) ? null : new InvalidFieldError()
   }
 }

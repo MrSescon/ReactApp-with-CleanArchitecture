ec793bce6e95f2ef2c881747b27ad66b0305cbde
Magno Sescon <magnosescon@gmail.com>
2021-01-31 10:01:19 -0300

    test: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -0,0 +1,15 @@
+import { ValidationComposite } from '@/validation/validators'
+import { makeSignUpValidation } from './signup-validation-factory'
+import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+
+describe('SignUpValidationfactory', () => {
+  test('Should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+    expect(composite).toEqual(ValidationComposite.build([
+      ...Builder.field('name').required().minLength(5).build(),
+      ...Builder.field('email').required().email().build(),
+      ...Builder.field('password').required().minLength(5).build(),
+      ...Builder.field('passwordConfirmation').required().minLength(5).build()
+    ]))
+  })
+})
